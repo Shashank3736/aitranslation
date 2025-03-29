7999eaafa46c3c4c5ab3e65d8409dc341d99d7d2
Shreyash Raj <shreyashr267@gmail.com>
2025-03-29 23:31:42 +0530

    small fixes

--- gemini.py
+++ gemini.py
@@ -1,12 +1,12 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
 genai.configure(api_key="AIzaSyATXjrPAriE7QUzFr6tYcP2iOrnaM9pioc")
-model = genai.GenerativeModel("gemini-2.0-flash-exp")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
-text = ""
 safetySettings = [
     {
       "category": HarmCategory.HARM_CATEGORY_HARASSMENT, "threshold": HarmBlockThreshold.BLOCK_NONE,
@@ -28,21 +28,48 @@ def get_content_from_url(url):
   soup = BeautifulSoup(response.text, 'html.parser')
   content_div = soup.find('div', class_='content')
   content_h1 = soup.find('h1', class_='headline')
+  content_body = soup.find('body')
 
-  if content_div:
-    return f"{content_h1.text}\n\n{content_div.text}"
-  else:
-    return ""
+  return content_body
+  # if content_div:
+  #   return f"{content_h1.text}\n\n{content_div.text}"
+  # else:
+  #   return ""
 
-text = get_content_from_url("https://m.shuhaige.net/375928/129589886.html")
+text = get_content_from_url("https://m.shuhaige.net/375928/129589887.html")
 
-print("Url fetched successfully.")
+def translate_using_ai(text, target_language, source_language):
+  prompt = f"""
+  I will provide you html element with {source_language} content. You have get the content from the element and translate into {target_language}. Then return the final translated content.
+  HTML Element: {text}
+  """
+  
+  response = model.generate_content(prompt)
+  return response.text
 
-prompt = f"""
-I will provide you html element with chinese content. You have get the content from the element and translate into english. Then return the final translated content.
-HTML Element: {text}
-"""
-response = model.generate_content(prompt)
+def translate_body(text, target_language, source_language):
+  prompt = f"""
+  I will provide you html body you have to do these stuff. you have to return 3 stuff in json format.
 
-with open('example.md', 'w', encoding='utf-8') as file:
-  file.write(response.text.replace('  ', ' '))
+  1. "next_chapter": Next chapter url parameter
+
+  2. "chapter_title": Chapter title translated in {target_language}
+
+  3. "chapter_content": Chapter content translated in {target_language}
+  
+  HTML Body: {text}
+  """
+
+  response = model.generate_content(prompt)
+  print(response.text)
+  try:
+    json_data = json.loads(response.text.replace("```json", "").replace("```", ""))
+    return json_data
+  except json.JSONDecodeError:
+    return {"error": "Invalid JSON format"}
+
+
+if __name__ == "__main__":
+
+  with open('example.md', 'w', encoding='utf-8') as file:
+    file.write(translate_body(text, "English", "Chinese")["chapter_content"].replace("\n", "\n\n"))
